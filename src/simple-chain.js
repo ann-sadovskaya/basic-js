f9da7d036c8c5b708a2ab2bb3f2702954a44eec9
Anna Sadouskaya <asadovskaya@elinext.com>
2020-02-18 15:49:54 +0300

    feat: resolve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,34 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof (position) !== 'number' || position - 1 < 0 || position - 1 >= this.getLength()) {
+      this.chain = [];
+      throw new Error();
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let fullChain = '';
+    for (let i = 0; i < this.getLength(); i++) {
+      fullChain += `( ${this.chain[i]} )`;
+      if (i < this.getLength() - 1) {
+        fullChain += '~~';
+      }
+    }
+    this.chain = [];
+    return fullChain;
   }
 };
 
